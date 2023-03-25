150006ced8c687778ba3476d5393d575fbc54b31
shivanibathani <shivanibathani19@gmail.com>
2023-03-25 13:05:20 -0400

    Final commit

--- Djangotest/Djangotest/urls.py
+++ Djangotest/Djangotest/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+     path('', include('pages.urls')),
     path('questionnaire/', include('questionnaire.urls')),
     path('admin/', admin.site.urls),
 ]
