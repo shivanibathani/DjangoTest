cb28d70c1b5633d5e16439e62247e819615ca16e
shivanibathani <shivanibathani19@gmail.com>
2023-03-25 14:39:16 -0400

    Final commit

--- Djangotest/Djangotest/settings.py
+++ Djangotest/Djangotest/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'questionnaire.apps.QuestionnaireConfig',
+     'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SHELL_PLUS = 'ipython'
--- Djangotest/questionnaire/models.py
+++ Djangotest/questionnaire/models.py
@@ -1,12 +1,27 @@
 from datetime import datetime, timezone
 from django.db import models
+from django.db.models.functions import Lower
+from django.forms import ValidationError
 
 # Create your models here.
 
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200, unique=True)
+
+    question_text = models.CharField(max_length=200)
+    question_text_lower = models.CharField(max_length=200, unique=True)
     pub_date = models.DateTimeField('date published')
+
+    def clean(self):
+        self.question_text_lower = self.question_text.lower()
+        existing_question = Question.objects.filter(
+            question_text_lower=self.question_text_lower).exclude(pk=self.pk).first()
+        if existing_question:
+            raise ValidationError("A question with this text already exists.")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super(Question, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.question_text
@@ -20,11 +35,17 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['question', 'choice_text'], name='unique_choice')
-        ]
+    def clean(self):
+        self.choice_text = self.choice_text.lower()
+        existing_choice = Choice.objects.filter(
+            question=self.question, choice_text=self.choice_text).exclude(pk=self.pk).first()
+        if existing_choice:
+            raise ValidationError(
+                "A choice with this text already exists for this question.")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super(Choice, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.choice_text
